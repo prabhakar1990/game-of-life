344982faa80512780e3d056a5af3675e0028a12c
prabhakar1990 <38628685+prabhakar1990@users.noreply.github.com>
2019-11-06 18:48:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -x
 cd /Users/pu20057981/Downloads/apache-tomcat-8.5.47/webapps
 rm -rf *.war
+rm -rf gameoflife
 cp /Users/Shared/Jenkins/Home/workspace/gol_pipeline3_pipelinescript_from_SCM/gameoflife-web/target/gameoflife.war .
 cd /Users/pu20057981/Downloads/apache-tomcat-8.5.47/bin
 ./shutdown.sh
