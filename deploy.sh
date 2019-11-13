ed847d72f628e58d97818cac1526581f66014829
prabhakar1990 <38628685+prabhakar1990@users.noreply.github.com>
2019-11-13 18:35:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
-cd /Users/pu20057981/Downloads/apache-tomcat-8.5.47/webapps
+cd /Users/pu20057981/apache-tomcat-8.5.47/webapps
 rm -rf *.war
 rm -rf gameoflife
 cp /Users/Shared/Jenkins/Home/workspace/gol_pipeline3_pipelinescript_from_SCM/gameoflife-web/target/gameoflife.war .
-cd /Users/pu20057981/Downloads/apache-tomcat-8.5.47/bin
+cd /Users/pu20057981/apache-tomcat-8.5.47/bin
 ./shutdown.sh
 sleep 5s
 JENKINS_NODE_COOKIE=dontKillMe sh startup.sh &
