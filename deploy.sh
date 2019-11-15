a16fea08471d737a89d73f91724884731902e92a
prabhakar1990 <38628685+prabhakar1990@users.noreply.github.com>
2019-11-15 18:36:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -x
 cd /Users/pu20057981/apache-tomcat-8.5.47/webapps
 rm -rf *.war
 rm -rf gameoflife
-cp /Users/Shared/Jenkins/Home/workspace/latestpipeline/gameoflife-web/target/gameoflife.war .
+cp /Users/Shared/Jenkins/Home/workspace/gameoflifefrestyle/gameoflife-web/target/gameoflife.war .
 cd /Users/pu20057981/apache-tomcat-8.5.47/bin
 ./shutdown.sh
 sleep 5s
